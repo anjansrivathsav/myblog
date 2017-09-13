d357512405baec81d00c3dfae9b4f32da133145a
Anjansrivathsav <anjansrivathsav1997@gmail.com>
2017-09-13 14:53:45 +0530

    Update script.js

--- blog/static/css/script.js
+++ blog/static/css/script.js
@@ -3,3 +3,8 @@ var locations;
 var largeInfoWindow;
 var map;
  var markers;
+function initMap() {
+
+// initializing the map with latitude and longitude positions
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: 17.9948584, lng: 79.580594},
