1415075b83ba0e85a8d2535bb318fffb786c1358
Anjansrivathsav <anjansrivathsav1997@gmail.com>
2017-09-13 14:54:11 +0530

    Update script.js

--- blog/static/css/script.js
+++ blog/static/css/script.js
@@ -8,3 +8,4 @@ function initMap() {
 // initializing the map with latitude and longitude positions
     map = new google.maps.Map(document.getElementById('map'), {
         center: {lat: 17.9948584, lng: 79.580594},
+      zoom: 15,
