ffb81d8e3cb674546be5e0706efcb224381c7995
Anjansrivathsav <anjansrivathsav1997@gmail.com>
2017-09-13 14:57:25 +0530

    Update script.js

--- blog/static/css/script.js
+++ blog/static/css/script.js
@@ -62,5 +62,31 @@ function initMap() {
                 featureType: 'road.highway',
                 elementType: 'labels.text.fill',
                 stylers: [{color: '#f3d19c'}]
+            }, {
+                featureType: 'transit',
+                elementType: 'geometry',
+                stylers: [{color: '#2f3948'}]
             },
+            {
+                featureType: 'transit.station',
+                elementType: 'labels.text.fill',
+                stylers: [{color: '#d59563'}]
+            },
+            {
+                featureType: 'water',
+                elementType: 'geometry',
+                stylers: [{color: '#17263c'}]
+            },
+            {
+                featureType: 'water',
+                elementType: 'labels.text.fill',
+                stylers: [{color: '#515c6d'}]
+            },
+            {
+                featureType: 'water',
+                elementType: 'labels.text.stroke',
+                stylers: [{color: '#17263c'}]
+            }
+        ]
+    });
        
