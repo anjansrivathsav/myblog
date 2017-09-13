029c0d661912586f0a6eea886be2b9f3d50e70ea
Anjansrivathsav <anjansrivathsav1997@gmail.com>
2017-09-13 14:55:12 +0530

    Update script.js

--- blog/static/css/script.js
+++ blog/static/css/script.js
@@ -9,3 +9,22 @@ function initMap() {
     map = new google.maps.Map(document.getElementById('map'), {
         center: {lat: 17.9948584, lng: 79.580594},
       zoom: 15,
+      styles: [
+            {elementType: 'geometry', stylers: [{color: '#242f3e'}]},
+            {elementType: 'labels.text.stroke', stylers: [{color: '#242f3e'}]},
+            {elementType: 'labels.text.fill', stylers: [{color: '#746855'}]},
+            {
+                featureType: 'administrative.locality',
+                elementType: 'labels.text.fill',
+                stylers: [{color: '#d59563'}]
+            },
+            {
+                featureType: 'poi',
+                elementType: 'labels.text.fill',
+                stylers: [{color: '#d59563'}]
+            },
+            {
+                featureType: 'poi.park',
+                elementType: 'geometry',
+                stylers: [{color: '#263c3f'}]
+            },
